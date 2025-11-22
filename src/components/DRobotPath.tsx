e41c9775bc4e64e6eab76777eb5b763bdc585f6b
Costas Basdekis <costas.basdekis@gmail.com>
2025-11-22 11:15:36 +0200

    Fix duplicate keys

--- src/components/DRobotPath.tsx
+++ src/components/DRobotPath.tsx
@@ -12,9 +12,9 @@ export function DRobotPath({robot, isSelected, robotPath}: DRobotPathProps) {
   const drawSettings = DrawSettings.use();
   return (
     <g className={"robot-path"}>
-      {robotPath.map(({previousPosition, position}) => (
+      {robotPath.map(({previousPosition, position}, index) => (
         <polygon
-          key={`${getPositionKey(previousPosition)}|${getPositionKey(position)}`}
+          key={`${robot.index}|${index}`}
           className={`robot-path-item index-${robot.index} ${isSelected ? "selected" : ""}`}
           points={`${drawSettings.getXYPositionStr(previousPosition.x + 0.5, previousPosition.y + 0.5)} ${drawSettings.getXYPositionStr(position.x + 0.5 , position.y + 0.5)}`}
         />
