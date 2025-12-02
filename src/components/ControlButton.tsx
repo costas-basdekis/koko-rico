8b55d3a528c805304b03fc518de1a7bf58c636e0
Costas Basdekis <costas.basdekis@gmail.com>
2025-12-02 13:53:47 +0000

    Fix not passing props to buttons & the like

--- src/components/ControlButton.tsx
+++ src/components/ControlButton.tsx
@@ -4,29 +4,41 @@ import { useMemo } from "react";
 export function ControlButton({
   children,
   ...props
-}: React.HTMLProps<HTMLButtonElement>) {
+}: JSX.IntrinsicElements["button"]) {
   const className = useMemo(() => {
     return `control-button ${props.className}`;
   }, [props.className]);
-  return <button className={className}>{children}</button>;
+  return (
+    <button {...props} className={className}>
+      {children}
+    </button>
+  );
 }
 
 export function ButtonRow({
   children,
   ...props
-}: React.HTMLProps<HTMLDivElement>) {
+}: JSX.IntrinsicElements["div"]) {
   const className = useMemo(() => {
     return `button-row ${props.className}`;
   }, [props.className]);
-  return <div className={className}>{children}</div>;
+  return (
+    <div {...props} className={className}>
+      {children}
+    </div>
+  );
 }
 
 export function ButtonLike({
   children,
   ...props
-}: React.HTMLProps<HTMLLabelElement>) {
+}: JSX.IntrinsicElements["label"]) {
   const className = useMemo(() => {
     return `button-like ${props.className}`;
   }, [props.className]);
-  return <label className={className}>{children}</label>;
+  return (
+    <label {...props} className={className}>
+      {children}
+    </label>
+  );
 }
