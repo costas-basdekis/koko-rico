f4a1beb5b9bbda44b3b1a8840df31d11ecd6a800
Costas Basdekis <costas.basdekis@gmail.com>
2025-12-12 14:21:25 +0000

    Fix `useResize` to listen to size changes

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,17 +1,17 @@
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 
 export function useWindowSize(
   fn: (windowWidth: number, windowHeight: number) => void,
   deps: React.DependencyList = [],
 ) {
-  useLayoutEffect(() => {
-    function updateSize() {
-      fn(window.innerWidth, window.innerHeight);
-    }
+  function updateSize() {
+    fn(window.innerWidth, window.innerHeight);
+  }
+  useEffect(() => {
     window.addEventListener("resize", updateSize);
-    updateSize();
     return () => {
       window.removeEventListener("resize", updateSize);
     };
   }, deps);
+  useLayoutEffect(updateSize, deps);
 }
